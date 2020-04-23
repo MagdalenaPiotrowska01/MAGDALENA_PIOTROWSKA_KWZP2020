12da74b3eef68ce99141bceb563da5e8e0fa01cf
MagdalenaPiotrowska01 <63207838+MagdalenaPiotrowska01@users.noreply.github.com>
2020-04-23 17:30:01 +0200

    Dodane zadanie
    zamykanie aplikacji przyciskiem zmienialnym

---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KWZP_2020
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btnClose.Enabled = false;
+            btnClose.Text = "Wpisz coś tam wyżej";
+        }
+
+
+        private void txtButtonChange_TextChanged(object sender, EventArgs e)
+        {
+            if (txtButtonChange.TextLength > 0)
+            {
+                btnClose.Text = txtButtonChange.Text;
+            }
+            else btnClose.Text = "wyłącz to po prostu";
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            btnClose.Enabled = chkButtonEnabled.Checked;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
